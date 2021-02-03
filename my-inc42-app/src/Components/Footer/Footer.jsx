cce6ffdbbda801fb21c8df3334cdf0a92e032342
Vinay Gupta <62155169+vinaytest75@users.noreply.github.com>
2021-02-03 23:22:23 +0530

    conflict resolve

--- my-inc42-app/src/Components/Footer/Footer.jsx
+++ my-inc42-app/src/Components/Footer/Footer.jsx
@@ -30,7 +30,7 @@ const Footer = () => {
   const classes = useStyles();
   return (
     <>
-      <AppBar position="fixed" className={classes.appBar}>
+      <AppBar position="relative" className={classes.appBar}>
         <img
           src="https://cdn.inc42.com/wp-content/uploads/2018/07/inc42-footer-logo-1-1.svg"
           alt="Logo"
