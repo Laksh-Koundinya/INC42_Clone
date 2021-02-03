eb94018b5c5881caee7ac7302b42d45b63d047df
Laksh-Koundinya <lakshmisn.08@gmail.com>
2021-02-03 09:23:21 +0530

    Featured Stories

--- my-inc42-app/src/App.js
+++ my-inc42-app/src/App.js
@@ -1,26 +1,13 @@
 import './App.css';
-<<<<<<< HEAD
-import CustomArrows from './Components/HomePageStart/HomeCarousel';
-
-
-
-=======
-
-import LoginModal from "./Components/Login_Signup/LoginModal"
-
-import { HomePage } from './Components/HomePageStart/HomePage';
->>>>>>> 34bdf9c7cb804e3c380a349c1ce5538947c5ea39
+import { FeaturedStories } from './Components/HomePageStart/FeaturedStories';
 
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-  <CustomArrows />
+  <FeaturedStories />
      
-=======
     
->>>>>>> 34bdf9c7cb804e3c380a349c1ce5538947c5ea39
     </div>
   );
 }
--- my-inc42-app/src/Components/HomePageStart/FeaturedStories.jsx
+++ my-inc42-app/src/Components/HomePageStart/FeaturedStories.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import CustomArrows from "./HomeCarousel";
 
 
 const FeaturedStories=()=>{
     return(
         <>
-        <div style = {{width:"100%", heigth:"70px",backgroundImage: "linear-gradient(90deg,#d81757 0%,#e4381b 100%)",color:"white",display:"flex"}}></div>
-        <div></div>
+        <div style = {{width:"100%",backgroundImage: "linear-gradient(90deg,#d81757 0%,#e4381b 100%)",color:"white",display:"flex",paddingTop:"20px",height:"320px"}}>
+            <h1>Featured Stories</h1>
+        </div>
+        <div style = {{marginTop:"-200px"}}>
+            <CustomArrows />
+        </div>
         </>
     )
 }
+export {FeaturedStories}
