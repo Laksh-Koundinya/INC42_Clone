f29fe178ba61b4d67f254a69bedd9b9f39db8343
anusha <ramaduguanusha123@gmail.com>
2021-02-04 15:38:26 +0530

    fix eslint

--- my-inc42-app/src/App.js
+++ my-inc42-app/src/App.js
@@ -6,10 +6,7 @@ import { Routes } from "./Routes/Routes";
 function App() {
 
   return (
-<<<<<<< HEAD
-=======
 
->>>>>>> a12552c3f263d413e216d21a1593c61b69fe5915
     <div className="App">
       <Routes />
     </div>
