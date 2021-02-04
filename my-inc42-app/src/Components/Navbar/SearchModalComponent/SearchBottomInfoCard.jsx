074233c1492e8400cedd7e757cc638f7972c604a
Vinay Gupta <62155169+vinaytest75@users.noreply.github.com>
2021-02-04 15:50:10 +0530

    pagination feature added in Search Modal

--- my-inc42-app/src/Components/Navbar/SearchModalComponent/SearchBottomInfoCard.jsx
+++ my-inc42-app/src/Components/Navbar/SearchModalComponent/SearchBottomInfoCard.jsx
@@ -1,13 +1,43 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import Box from "@material-ui/core/Box";
 import Typography from "@material-ui/core/Typography";
 
+const useStyles = makeStyles({
+  root: {
+    width: "300px",
+    height: "auto",
+    wordWrap: "break-word",
+    margin: "10px",
+    padding: "10px",
+    "&:hover": {
+      backgroundColor: "#f4f4f4",
+    },
+  },
+  head: {
+    color: "red",
+    fontWeight: "700",
+    fontSize: "12px",
+  },
+  content: {
+    fontWeight: "700",
+    fontSize: "15px",
+  },
+  foot: {
+    color: "grey",
+  },
+});
+
 const SearchBottomInfoCard = () => {
+  const classes = useStyles();
   return (
-    <Box>
-      <Typography>NEWS</Typography>
-      <Typography>Content</Typography>
-      <span>Author - date - Time to read</span>
+    <Box className={classes.root}>
+      <Typography className={classes.head}>NEWS</Typography>
+      <Typography className={classes.content} variant="button">
+        ContentContentContentContentContentContentContentContentContentContent
+      </Typography>
+      <br />
+      <span className={classes.foot}>By Author - date - Time to read</span>
     </Box>
   );
 };
