ee385059c15a241607d23b648f28cd0d6fe2a9dc
anusha <ramaduguanusha123@gmail.com>
2021-02-05 10:28:32 +0530

    completed accoutnPage

--- my-inc42-app/src/Components/Pagination/Pagination.jsx
+++ my-inc42-app/src/Components/Pagination/Pagination.jsx
@@ -1,27 +1,30 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import Pagination from '@material-ui/lab/Pagination';
-
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Pagination from "@material-ui/lab/Pagination";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    '& > * + *': {
-      marginTop: theme.spacing(2),
-      display:"flex",
-      justifyContent:"center"
+    root: {
+        "& > * + *": {
+            marginTop: theme.spacing(2),
+            display: "flex",
+            justifyContent: "center",
+        },
     },
-  },
 }));
 
+export function PaginationRounded({ page, total, handlePageChange }) {
+    const classes = useStyles();
 
-export function PaginationRounded({page,total, handlePageChange}) {
-  const classes = useStyles();
-
-  return (
-    <div className={classes.root}>
-    
-      <Pagination count={total } page={page} onChange={handlePageChange} />
-    </div>
-  );
+    return (
+        <div className={classes.root}>
+            <Pagination 
+             color="secondary"
+             variant="outlined"
+             shape="rounded"
+             count={total} 
+             page={page} 
+             onChange={handlePageChange} />
+        </div>
+    );
 }
