c9a742d5f7ffa7011253b4400e5df033b3490e2e
Laksh-Koundinya <lakshmisn.08@gmail.com>
2021-02-05 13:30:15 +0530

    completed navigation to particular news page

--- my-inc42-app/src/Components/HomePageStart/NewsGrid.jsx
+++ my-inc42-app/src/Components/HomePageStart/NewsGrid.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect,useSelector } from "react"
 import { useDispatch } from "react-redux"
+import { useHistory } from "react-router-dom";
 import { getNewsData } from "../../Redux/actionCreators";
 import { NormalCard } from "./Cards/NormalCard";
 import styles from "./gridDisplay.module.css"
@@ -7,14 +8,25 @@ import styles from "./gridDisplay.module.css"
 export const NewsGrid=(props)=>{
     
     const {data} = props.data
+    const history = useHistory()
     
 console.log(data)
+const goToDisplayNews = (id,data) =>{
+    const location = {
+        pathName: `/news-details/${id}`,
+        state:{
+            data:data
+        }
+    }
+    console.log(location,"loc")
+    history.push( location.pathName )
+}
 
    
     return (
         <div className = {styles.gridDisplay}>
              {data && data.filter((item,i)=> i < 9)
-          .map((item) =>  <NormalCard key = {item.id} data = {item} />)
+          .map((item) =>  <NormalCard key = {item.id} data = {item}  goToDisplayNews = {  goToDisplayNews }/>)
           }
       
             </div>
